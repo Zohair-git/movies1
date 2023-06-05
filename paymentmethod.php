3bd71f1e043ba2f86a6d054f2a531b58f189d2d6
Zohair Adeel <zohairadeel987@gmail.com>
2023-06-05 19:51:27 +0500

    update

--- paymentmethod.php
+++ paymentmethod.php
@@ -194,22 +194,7 @@ include('connection.php');
     <!-- ==========Movie-Section========== -->
 
     <!-- ==========Newslater-Section========== -->
-    <footer class="footer-section">
-        <div class="newslater-section padding-bottom">
-            <div class="container">
-                <div class="newslater-container bg_img" data-background="./assets/images/newslater/newslater-bg01.jpg">
-                    <div class="newslater-wrapper">
-                        <h5 class="cate">subscribe to Boleto </h5>
-                        <h3 class="title">to get exclusive benifits</h3>
-                        <form class="newslater-form">
-                            <input type="text" placeholder="Your Email Address">
-                            <button type="submit">subscribe</button>
-                        </form>
-                        <p>We respect your privacy, so we never share your info</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+   
         <div class="container">
             <div class="footer-top">
                 <div class="logo">
